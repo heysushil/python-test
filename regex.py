67e3e40b96464384a60ee7dc97feb3062a1c8188
heysushilm <sushil.chaudhary111@gmail.com>
2020-03-02 11:43:21 +0530

    patterns definetion

--- regex.py
+++ regex.py
@@ -18,6 +18,56 @@ You can complement (invert) the character set by using caret ^ symbol at the sta
 
 [^abc] means any character except a or b or c.
 [^0-9] means any non-digit character.
+
+. - Period
+
+A period matches any single character (except newline '\n').
+
+^ - Caret
+
+The caret symbol ^ is used to check if a string starts with a certain character.
+
+$ - Dollar
+
+The dollar symbol $ is used to check if a string ends with a certain character.
+
+* - Star
+
+The star symbol * matches zero or more occurrences of the pattern left to it.
+
++ - Plus
+
+The plus symbol + matches one or more occurrences of the pattern left to it.
+
+? - Question Mark
+
+The question mark symbol ? matches zero or one occurrence of the pattern left to it.
+
+{} - Braces
+
+Consider this code: {n,m}. This means at least n, and at most m repetitions of the pattern left to it.
+
+| - Alternation
+
+Vertical bar | is used for alternation (or operator).
+
+() - Group
+
+Parentheses () is used to group sub-patterns. For example, (a|b|c)xz match any string that matches either a or b or c followed by xz
+
+\ - Backslash
+
+Backlash \ is used to escape various characters including all metacharacters. For example,
+
+\$a match if a string contains $ followed by a. Here, $ is not interpreted by a RegEx engine in a special way.
+
+If you are unsure if a character has special meaning or not, you can put \ in front of it. This makes sure the character is not treated in a special way.
+
+'''
+
+'''
+----------------Special Sequences--------------------------
+
 '''
 import re
 pattern = '^a...s$'
